db1d9b77603772a1718a8cff2de1df3399e4a9d8
Charlie Anderson <charliepjanderson@gmail.com>
2020-02-08 18:46:44 +0000

    adds instrument class

--- src/main/java/instruments/Instrument.java
+++ src/main/java/instruments/Instrument.java
@@ -1,4 +1,51 @@
 package instruments;
 
-public class Instrument {
+import behaviours.IPlay;
+
+public abstract class Instrument implements IPlay {
+    private double buyingPrice;
+    private double sellingPrice;
+    private String name;
+    private String make;
+    private String model;
+    private String sound;
+
+    public Instrument(String name, String make, String model, String sound, double buyingPrice, double sellingPrice) {
+        this.name = name;
+        this.make = make;
+        this.model = model;
+        this.sound = sound;
+        this.buyingPrice = buyingPrice;
+        this.sellingPrice = sellingPrice;
+    }
+
+    public double getBuyingPrice() {
+        return this.buyingPrice;
+    }
+
+    public double getSellingPrice() {
+        return this.sellingPrice;
+    }
+
+    public String getName() {
+        return this.name;
+    }
+
+    public String getMake() {
+        return this.make;
+    }
+
+    public String getModel() {
+        return this.model;
+    }
+
+    public String getSound() {
+        return this.sound;
+    }
+
+    public void setSellingPrice(double newPrice) {
+        this.sellingPrice = newPrice;
+    }
+
+    public abstract String playInstrument();
 }
