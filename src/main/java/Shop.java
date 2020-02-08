c3247e39fcf86df78231641587990affcda39bd3
Charlie Anderson <charliepjanderson@gmail.com>
2020-02-08 22:06:51 +0000

    adds shop class and passes tests

--- src/main/java/Shop.java
+++ src/main/java/Shop.java
@@ -1,2 +1,34 @@
+import behaviours.ISell;
+
+import java.util.ArrayList;
+
 public class Shop {
+    private ArrayList<ISell> stock;
+
+    public Shop() {
+        this.stock = new ArrayList<ISell>();
+    }
+
+    public void addItem(ISell newItem) {
+        this.stock.add(newItem);
+    }
+
+    public void removeItem(ISell oldItem) {
+        this.stock.remove(oldItem);
+    }
+
+    public int getSizeOfStock() {
+        return this.stock.size();
+    }
+
+    public double getProjectedProfits() {
+        double profits = 0;
+
+        for(ISell item : this.stock) {
+            double itemMarkup = item.calculateMarkUp();
+            profits += itemMarkup;
+        }
+
+        return profits;
+    }
 }
--- src/test/java/ShopTest.java
+++ src/test/java/ShopTest.java
@@ -1,2 +1,53 @@
+import accessories.Instruments;
+import accessories.Item;
+import instruments.Percussion;
+import instruments.PercussionInstruments;
+import instruments.Woodwind;
+import instruments.WoodwindInstrument;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
 public class ShopTest {
+    private Shop newShop;
+    private Item newItem;
+    private Woodwind saxophone;
+    private Percussion piano;
+
+    @Before
+    public void before() {
+        this.newShop = new Shop();
+        this.newItem = new Item("Reed", "Reed 5 for Saxophone", Instruments.SAXOPHONE, 3.00, 6.00);
+        this.saxophone = new Woodwind("Yamaha", "SAX123", 10, 300.00, 400.00, WoodwindInstrument.SAXOPHONE);
+        this.piano = new Percussion("Yamaha", "ClassicFM", 200, 2000.00, 3000.00, PercussionInstruments.PIANO);
+    }
+
+    @Test
+    public void shopStartsEmpty() {
+        assertEquals(0, newShop.getSizeOfStock());
+    }
+
+    @Test
+    public void canAddItem() {
+        newShop.addItem(saxophone);
+        assertEquals(1, newShop.getSizeOfStock());
+    }
+
+    @Test
+    public void canRemoveItem() {
+        newShop.addItem(saxophone);
+        newShop.addItem(piano);
+        newShop.addItem(newItem);
+        newShop.removeItem(saxophone);
+        assertEquals(2, newShop.getSizeOfStock());
+    }
+
+    @Test
+    public void canGetProjectedProfits() {
+        newShop.addItem(saxophone);
+        newShop.addItem(piano);
+        newShop.addItem(newItem);
+        assertEquals(1103.00, newShop.getProjectedProfits(), 0.00);
+    }
 }
